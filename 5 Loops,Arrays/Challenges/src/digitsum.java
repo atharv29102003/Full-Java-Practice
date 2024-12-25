49d9b97d28dbb255cc0dd7cee386cc239877c28d
atharv29102003 <93941902+atharv29102003@users.noreply.github.com>
2024-12-26 01:00:37 +0530

    This program calculates and displays the sum of the digits of a given integer. It takes an input number, processes each digit individually by converting it into a numeric value, and accumulates the sum of these digits.

--- 5 Loops,Arrays/Challenges/src/digitsum.java
+++ 5 Loops,Arrays/Challenges/src/digitsum.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class digitsum {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your Number: ");
+        int num = input.nextInt();
+        System.out.println("Sum  of the digits of the number " + (num) + " is " + (sumofdigits(num)));
+        input.close();
+        input.close();
+    }
 
+    public static int sumofdigits(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
     }
 }
